e420cc65530607bb5302ddbc11ea01e6cf6480a1
CDev-coder <Christopher.Cheng87@gmail.com>
2025-08-18 23:36:55 -0400

    Updated to Chap 14

--- basic-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ basic-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,7 @@
 import EditForm from "@/app/ui/invoices/edit-form";
 import Breadcrumbs from "@/app/ui/invoices/breadcrumbs";
 import { fetchInvoiceById, fetchCustomers } from "@/app/lib/data";
+import { notFound } from "next/navigation";
 
 export default async function Page(props: { params: Promise<{ id: string }> }) {
   const params = await props.params;
@@ -9,6 +10,12 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
     fetchInvoiceById(id),
     fetchCustomers(),
   ]);
+
+  ///Handles a 404 Error
+  if (!invoice) {
+    notFound(); //This triggers Next.js to look for the closest 'not-found' file in this page.tsx structure. In this case the one in the [id]/edit.
+  }
+  ///We can have a global version if we want to handle more than one special case.
   return (
     <main>
       <Breadcrumbs
--- basic-dashboard/app/lib/actions.ts
+++ basic-dashboard/app/lib/actions.ts
@@ -15,7 +15,10 @@ const FormSchema = z.object({
   status: z.enum(["pending", "paid"]),
   date: z.string(),
 });
+
+////////CREATING INVOICE
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get("customerId"),
@@ -26,11 +29,55 @@ export async function createInvoice(formData: FormData) {
   const date = new Date().toISOString().split("T")[0];
 
   /////Once we have the database, we send over our new data/insert our new data into the database
-  await sql`
-    INSERT INTO invoices (customer_id, amount, status, date)
-    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-  `;
-
+  ////This try & catch will attempt to find the sql
+  try {
+    await sql`
+      INSERT INTO invoices (customer_id, amount, status, date)
+      VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+    `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
   revalidatePath("/dashboard/invoices"); //Refreshes any client data for the invoices to the newest instance.
-  redirect("/dashboard/invoices"); ////Then we refresh the current page after the new data has been created
+  redirect("/dashboard/invoices"); ////Then we refresh the current page after the new data has been created.
+  ///"redirect" throws a unique error code that will still be detected by a regular try & catch.
+}
+
+//////// UPDATING INVOICE
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get("customerId"),
+    amount: formData.get("amount"),
+    status: formData.get("status"),
+  });
+
+  const amountInCents = amount * 100;
+
+  try {
+    await sql`
+        UPDATE invoices
+        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+        WHERE id = ${id}
+      `;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
+
+  revalidatePath("/dashboard/invoices"); ///Same as the create form
+  redirect("/dashboard/invoices");
+}
+
+///DELETING INVOICE
+export async function deleteInvoice(id: string) {
+  try {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+  } catch (error) {
+    // We'll log the error to the console for now
+    console.error(error);
+  }
+  revalidatePath("/dashboard/invoices"); //Just refresh since where the delete is located is the invoice page
 }
